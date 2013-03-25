0a06e91f0b7a5acff93ce11c5070d7945312a92d
David Eads <davideads@gmail.com>
2013-03-25 11:42:32 -0500

    add fab file

---
+++ fabfile.py
@@ -0,0 +1,199 @@
+# Chicago Tribune News Applications fabfile
+# No copying allowed
+
+from fabric.api import *
+
+"""
+Base configuration
+"""
+#name of the deployed site if different from the name of the project
+env.site_name = 'pandascrapers'
+
+env.project_name = 'pandascrapers'
+env.path = '/home/newsapps/sites/%(project_name)s' % env
+env.log_path = '/home/newsapps/logs/%(project_name)s' % env
+env.env_path = '%(path)s/env' % env
+env.repo_path = '%(path)s/repository' % env
+env.apache_config_path = '/home/newsapps/sites/apache/%(project_name)s' % env
+env.python = 'python2.6'
+env.repository_url = 'git@github.com:newsapps/pandascrapers.git' % env
+
+"""
+Environments
+"""
+def production():
+    """
+    Work on production environment
+    """
+    env.settings = 'production'
+    env.hosts = ['bfcron.tribapps.com']
+    env.user = 'newsapps'
+
+def staging():
+    """
+    Work on staging environment
+    """
+    env.settings = 'staging'
+    env.hosts = ['db.beta.tribapps.com'] 
+    env.user = 'newsapps'
+
+"""
+Branches
+"""
+def stable():
+    """
+    Work on stable branch.
+    """
+    env.branch = 'stable'
+
+def master():
+    """
+    Work on development branch.
+    """
+    env.branch = 'master'
+
+def branch(branch_name):
+    """
+    Work on any specified branch.
+    """
+    env.branch = branch_name
+
+"""
+Commands - setup
+"""
+def setup():
+    """
+    Setup a fresh virtualenv, install everything we need, and fire up the database.
+    
+    Does NOT perform the functions of deploy().
+    """
+    _confirm_branch()
+    
+    require('settings', provided_by=[production, staging])
+    require('branch', provided_by=[stable, master, branch])
+    
+    setup_directories()
+    setup_virtualenv()
+    clone_repo()
+    checkout_latest()
+    install_requirements()
+    install_apache_conf()
+
+def setup_directories():
+    """
+    Create directories necessary for deployment.
+    """
+    run('mkdir -p %(path)s' % env)
+    run('mkdir -p %(env_path)s' % env)
+    run('mkdir -p %(log_path)s;' % env)
+    sudo('chgrp -R www-data %(log_path)s; chmod -R g+w %(log_path)s;' % env)
+    
+    with settings(warn_only=True):
+        run('ln -s %(log_path)s %(path)s/logs' % env)
+    
+def setup_virtualenv():
+    """
+    Setup a fresh virtualenv.
+    """
+    run('virtualenv -p %(python)s --no-site-packages %(env_path)s;' % env)
+    run('source %(env_path)s/bin/activate; easy_install -U setuptools; easy_install pip;' % env)
+
+def clone_repo():
+    """
+    Do initial clone of the git repository.
+    """
+    with settings(warn_only=True):
+        run('git clone %(repository_url)s %(repo_path)s' % env)
+
+def checkout_latest():
+    """
+    Pull the latest code on the specified branch.
+    """
+    run('cd %(repo_path)s; git fetch; git checkout %(branch)s; git pull origin %(branch)s' % env)
+
+def install_requirements():
+    """
+    Install the required packages using pip.
+    """
+    run('source %(env_path)s/bin/activate; pip install -U -E %(env_path)s -r %(repo_path)s/requirements.txt' % env)
+
+def install_apache_conf():
+    """
+    Install the apache site config file.
+    """
+    sudo('cp -T %(repo_path)s/schools/configs/%(settings)s/apache %(apache_config_path)s' % env)
+
+
+"""
+Commands - deployment
+"""
+def deploy():
+    """
+    Deploy the latest version of the site to the server and restart Apache2.
+    
+    Does not perform the functions of load_new_data().
+    """
+    _confirm_branch()
+    
+    require('settings', provided_by=[production, staging])
+    require('branch', provided_by=[stable, master, branch])
+    
+    with settings(warn_only=True):
+        maintenance_up()
+        
+    checkout_latest()
+    maintenance_down()
+    
+def maintenance_up():
+    """
+    Install the Apache maintenance configuration.
+    """
+    sudo('cp -T %(repo_path)s/schools/configs/%(settings)s/apache_maintenance %(apache_config_path)s' % env)
+    reboot()
+
+def reboot(): 
+    """
+    Restart the Apache2 server.
+    """
+    sudo('service apache2 restart')
+    
+def maintenance_down():
+    """
+    Reinstall the normal site configuration.
+    """
+    install_apache_conf()
+    reboot()
+    
+
+"""
+Commands - miscellaneous
+"""
+def echo_host():
+    """
+    Echo the current host to the command line.
+    """
+    run('echo %(settings)s; echo %(hosts)s' % env)
+
+"""
+Deaths, destroyers of worlds
+"""
+def shiva_the_destroyer():
+    """
+    Remove all directories, databases, etc. associated with the application.
+    """
+    with settings(warn_only=True):
+        run('rm -Rf %(path)s' % env)
+        run('rm -Rf %(log_path)s' % env)
+        # TODO: destroy DB
+        sudo('rm %(apache_config_path)s' % env)
+        reboot()
+        run('s3cmd del --recursive s3://%(s3_bucket)s/%(project_name)s' % env)
+
+"""
+Utility functions (not to be called directly)
+"""
+def _confirm_branch():
+    if (env.settings == 'production' and env.branch != 'stable'):
+        answer = prompt("You are trying to deploy the '%(branch)s' branch to production.\nYou should really only deploy a stable branch.\nDo you know what you're doing?" % env, default="Not at all")
+        if answer not in ('y','Y','yes','Yes','buzz off','screw you'):
+            exit()
